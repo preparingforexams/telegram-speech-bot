2ecd87675e04833464fe58ce590a98963fb553cd
Björn Petersen <git@bjoernpetersen.net>
2022-12-22 17:26:25 +0100

    Request neural2

--- src/bob/infrastructure/adapters/text_to_speech/gcp.py
+++ src/bob/infrastructure/adapters/text_to_speech/gcp.py
@@ -1,5 +1,3 @@
-import asyncio
-
 from google.cloud import texttospeech
 
 from bob.application.ports import TextToSpeech
@@ -7,23 +5,21 @@ from bob.application.ports import TextToSpeech
 
 class GcpTextToSpeech(TextToSpeech):
     def __init__(self):
-        self._client = texttospeech.TextToSpeechClient()
+        self._client = texttospeech.TextToSpeechAsyncClient()
 
     async def convert_to_speech(self, text: str) -> bytes:
         synth_input = texttospeech.SynthesisInput(text=text)
         voice = texttospeech.VoiceSelectionParams(
             language_code="de-DE",
             ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL,
+            name="de-DE-Neural2-F",
         )
         audio_config = texttospeech.AudioConfig(
             audio_encoding=texttospeech.AudioEncoding.OGG_OPUS
         )
-        response = await asyncio.get_running_loop().run_in_executor(
-            None,
-            lambda: self._client.synthesize_speech(
-                input=synth_input,
-                voice=voice,
-                audio_config=audio_config,
-            ),
+        response = await self._client.synthesize_speech(
+            input=synth_input,
+            voice=voice,
+            audio_config=audio_config,
         )
         return response.audio_content
