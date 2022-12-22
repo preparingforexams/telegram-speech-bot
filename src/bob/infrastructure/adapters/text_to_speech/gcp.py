4ae524e516ec67b1f0716adf76016cc78d0189ff
Björn Petersen <git@bjoernpetersen.net>
2022-12-22 17:34:33 +0100

    Attempt to fix loop situation

--- src/bob/infrastructure/adapters/text_to_speech/gcp.py
+++ src/bob/infrastructure/adapters/text_to_speech/gcp.py
@@ -5,9 +5,18 @@ from bob.application.ports import TextToSpeech
 
 class GcpTextToSpeech(TextToSpeech):
     def __init__(self):
-        self._client = texttospeech.TextToSpeechAsyncClient()
+        self._client: texttospeech.TextToSpeechAsyncClient | None = None
+
+    def _get_client(self) -> texttospeech.TextToSpeechAsyncClient:
+        client = self._client
+        if client is not None:
+            return client
+
+        self._client = (client := texttospeech.TextToSpeechAsyncClient())
+        return client
 
     async def convert_to_speech(self, text: str) -> bytes:
+        client = self._get_client()
         synth_input = texttospeech.SynthesisInput(text=text)
         voice = texttospeech.VoiceSelectionParams(
             language_code="de-DE",
@@ -17,7 +26,7 @@ class GcpTextToSpeech(TextToSpeech):
         audio_config = texttospeech.AudioConfig(
             audio_encoding=texttospeech.AudioEncoding.OGG_OPUS
         )
-        response = await self._client.synthesize_speech(
+        response = await client.synthesize_speech(
             input=synth_input,
             voice=voice,
             audio_config=audio_config,
