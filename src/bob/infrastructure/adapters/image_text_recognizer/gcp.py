5c58252678347b23230f0be5636ed0f67d02611d
Björn Petersen <git@bjoernpetersen.net>
2022-12-29 18:27:36 +0100

    Implement GCP OCR

--- src/bob/infrastructure/adapters/image_text_recognizer/gcp.py
+++ src/bob/infrastructure/adapters/image_text_recognizer/gcp.py
@@ -1,11 +1,21 @@
 import logging
 
+from asyncache import cached
+from google.cloud.vision import ImageAnnotatorAsyncClient, Image
+
 from bob.application.ports import ImageTextRecognizer
 
 _LOG = logging.getLogger(__name__)
 
 
 class GoogleImageTextRecognizer(ImageTextRecognizer):
+    @cached({})
+    async def _get_client(self) -> ImageAnnotatorAsyncClient:
+        return ImageAnnotatorAsyncClient()
+
     async def detect_text(self, image_url: str) -> str | None:
-        # TODO
-        return None
+        client = await self._get_client()
+        image = Image()
+        image.source.image_uri = image_url
+        response = await client.text_detection(image=image)  # type: ignore
+        return response.full_text_annotation.text
