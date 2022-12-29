779cecec2be70dc1af6f9cab0fd9fc1c23824e74
Björn Petersen <git@bjoernpetersen.net>
2022-12-29 19:59:59 +0100

    Enable OCR everywhere

--- src/bob/infrastructure/repos/chat/static.py
+++ src/bob/infrastructure/repos/chat/static.py
@@ -10,13 +10,16 @@ class StaticChatRepository(ChatRepository):
             Chat(
                 id=133399998,
                 enable_ocr=True,
+                enable_inline_options=True,
             ),
             Chat(
                 id=-1001348149915,
+                enable_ocr=True,
                 delete_text_message=True,
             ),
             Chat(
                 id=-1001243399669,
+                enable_ocr=True,
                 enable_inline_options=True,
             ),
         }
